e25014f866336886c0dbff4af832053eca5ae89d
Developer <unitygamesxojak@gmail.com>
2019-12-25 16:47:25 +0500

    basic-start

---
+++ src/Components/app/app.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './header';
+import Container from './container';
+import Gallery from './gallery';
+import Footer from './footer';
+import './app.css';
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <Container />
+      <Gallery />
+      <Footer />
+    </React.Fragment>
+  );
+}
+
+export default App;
